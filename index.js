234ce0fa08da6c11f03fa6afd783509b9e2b791d
Matheus Mösken Diegues <matheus.diegues@fatec.sp.gov.br>
2024-04-04 19:18:35 -0300

    Adiciona arquivos de visualização para clientes, pedidos e produtos

--- index.js
+++ index.js
@@ -7,15 +7,48 @@ app.get('/', (req, res) => {
     res.render('index');
     });
 
-app.get('/clientes', (req, res) => {
-    res.render('clientes');
-    });
-app.get('/produtos', (req, res) => {
-    res.render('produtos');
-    });
-app.get('/pedidos', (req, res) => {
-    res.render('pedidos');
-    });
+// ROTA PRODUTOS
+app.get("/produtos/:produto?", (req, res) => {
+    let produto = req.params.produto
+    // Array com os produtos
+    let produtos = ['Computador', 'Celular', 'Tablet', 'Notebook']
+    res.render('produtos', {
+        // Enviando variáveis para página
+        produto : produto,
+        produtos : produtos
+    })
+})
+
+// ROTA PEDIDOS
+app.get("/pedidos", (req, res) => {
+    // Array de objetos com os pedidos
+    let pedidos = [
+        {produto: "Celular", preco: 12000}, 
+        {produto: "Computador", preco: 3000}, 
+        {produto: "Tablet", preco: 2000}, 
+        {produto: "Notebook", preco: 3800}
+    ]
+    res.render('pedidos', {
+        // Enviando o array de objetos para página
+        pedidos : pedidos
+    })
+})
+
+// ROTA CLIENTES
+app.get("/clientes", (req, res) => {
+    // Array de objetos com os pedidos
+    let clientes = [
+        {nome: "João Pereira", cpf: "111.111.111-00", cidade: "Registro"}, 
+        {nome: "Ana Souza", cpf: "222.222.222-00", cidade: "Juquiá"}, 
+        {nome: "Caio César", cpf: "333.333.333-00", cidade: "Pariquera"}, 
+        {nome: "Felipe Mendes", cpf: "444.444.444-00", cidade: "Miracatu"}
+    ]
+    res.render('clientes', {
+        // Enviando o array de objetos para página
+        clientes : clientes
+    })
+})
+
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
